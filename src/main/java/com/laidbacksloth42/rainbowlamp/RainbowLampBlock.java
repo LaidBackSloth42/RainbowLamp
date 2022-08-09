ca8587864c94e34d3d85bcbca08dc4336ff0d17f
LaidBackSloth42 <109034679+LaidBackSloth@users.noreply.github.com>
2022-08-09 17:58:02 +0200

    1.1 update

--- src/main/java/com/laidbacksloth42/rainbowlamp/RainbowLampBlock.java
+++ src/main/java/com/laidbacksloth42/rainbowlamp/RainbowLampBlock.java
@@ -1,6 +1,9 @@
 package com.laidbacksloth42.rainbowlamp;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -8,35 +11,50 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 
 public class RainbowLampBlock extends Block {
     public static final IntegerProperty POWER = BlockStateProperties.POWER;
+    public static final IntegerProperty COLOR = IntegerProperty.create("color", 0, 15);
 
     public RainbowLampBlock(Properties p_49795_) {
         super(p_49795_);
-        this.registerDefaultState(this.defaultBlockState().setValue(POWER, 0));
+        this.registerDefaultState(this.defaultBlockState().setValue(POWER, 0).setValue(COLOR, 0));
     }
 
     @Nullable
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext pContext) {
-        return this.defaultBlockState().setValue(POWER, pContext.getLevel().getBestNeighborSignal(pContext.getClickedPos()));
+        int power = pContext.getLevel().getBestNeighborSignal(pContext.getClickedPos());
+        return this.defaultBlockState().setValue(POWER, power).setValue(COLOR, power);
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
         pBuilder.add(POWER);
+        pBuilder.add(COLOR);
     }
 
     @Override
     public void neighborChanged(BlockState pState, Level pLevel, BlockPos pPos, Block pBlock, BlockPos pFromPos, boolean pIsMoving) {
-        if (!pLevel.isClientSide) {
-            int flag = pState.getValue(POWER);
-            if (flag != pLevel.getBestNeighborSignal(pPos)) {
-                flag = pLevel.getBestNeighborSignal(pPos);
-                pLevel.setBlock(pPos, pState.setValue(POWER, flag), 2);
-            }
+        if (!pLevel.isClientSide && pState.getValue(POWER) != pLevel.getBestNeighborSignal(pPos)) {
+            int power = pLevel.getBestNeighborSignal(pPos);
+            pLevel.setBlock(pPos, pState.setValue(POWER, power).setValue(COLOR, power), 2);
         }
+    }
+
+    @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        if (!pLevel.isClientSide && !pLevel.hasNeighborSignal(pPos) && pHand == InteractionHand.MAIN_HAND && !pPlayer.isShiftKeyDown()) {
+            int color = pState.getValue(COLOR);
+            if (color < 15) {
+                color++;
+            } else {
+                color = 0;
+            }
+            pLevel.setBlock(pPos, pState.setValue(COLOR, color), 2);
+        }
+        return InteractionResult.SUCCESS;
     }
 }
