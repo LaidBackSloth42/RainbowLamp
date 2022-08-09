ec1a4bc7be334163ce297b7d4362d36c7eed23ba
LaidBackSloth42 <109034679+LaidBackSloth@users.noreply.github.com>
2022-08-09 17:59:55 +0200

    1.1 update

--- src/main/java/com/laidbacksloth42/rainbowlamp/BlockRegistry.java
+++ src/main/java/com/laidbacksloth42/rainbowlamp/BlockRegistry.java
@@ -18,7 +18,7 @@ import net.minecraft.world.BlockView;
 public class BlockRegistry {
     public static final Block RAINBOW_LAMP_BLOCK = Registry.register(Registry.BLOCK, new Identifier(RainbowLamp.MOD_ID, "rainbow_lamp"),
             new RainbowLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).strength(0.3F).sounds(BlockSoundGroup.GLASS).allowsSpawning(BlockRegistry::always)
-                    .luminance((state) -> state.get(RainbowLampBlock.POWER) == 0 ? 0 : 15)));
+                    .luminance((state) -> state.get(RainbowLampBlock.COLOR) == 0 ? 0 : 15)));
 
     public static final Item RAINBOW_LAMP_ITEM = Registry.register(Registry.ITEM, new Identifier(RainbowLamp.MOD_ID, "rainbow_lamp"),
             new BlockItem(RAINBOW_LAMP_BLOCK, new FabricItemSettings().group(ItemGroup.REDSTONE)));
--- src/main/java/com/laidbacksloth42/rainbowlamp/RainbowLampBlock.java
+++ src/main/java/com/laidbacksloth42/rainbowlamp/RainbowLampBlock.java
@@ -2,45 +2,64 @@ package com.laidbacksloth42.rainbowlamp;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class RainbowLampBlock extends Block {
     public static final IntProperty POWER;
+    public static final IntProperty COLOR;
 
     public RainbowLampBlock(Settings settings) {
         super(settings);
-        this.setDefaultState(this.getDefaultState().with(POWER, 0));
+        this.setDefaultState(this.getDefaultState().with(POWER, 0).with(COLOR, 0));
     }
 
     @Nullable
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState().with(POWER, ctx.getWorld().getReceivedRedstonePower(ctx.getBlockPos()));
+        int power = ctx.getWorld().getReceivedRedstonePower(ctx.getBlockPos());
+        return this.getDefaultState().with(POWER, power).with(COLOR, power);
     }
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(POWER);
+        builder.add(COLOR);
     }
 
     @Override
     public void neighborUpdate(BlockState state, World world, BlockPos pos, Block block, BlockPos fromPos, boolean notify) {
-        if (!world.isClient) {
-            int flag = state.get(POWER);
-            if (flag != world.getReceivedRedstonePower(pos)) {
-                flag = world.getReceivedRedstonePower(pos);
-                world.setBlockState(pos, state.with(POWER, flag), 2);
-            }
+        if (!world.isClient && state.get(POWER) != world.getReceivedRedstonePower(pos)) {
+            int power = world.getReceivedRedstonePower(pos);
+            world.setBlockState(pos, state.with(POWER, power).with(COLOR, power), 2);
         }
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!world.isClient && !world.isReceivingRedstonePower(pos) && hand == Hand.MAIN_HAND && !player.isSneaking()) {
+            int color = state.get(COLOR);
+            if (color < 15) {
+                color++;
+            } else {
+                color = 0;
+            }
+            world.setBlockState(pos, state.with(COLOR, color), 2);
+        }
+        return ActionResult.SUCCESS;
     }
 
     static {
         POWER = Properties.POWER;
+        COLOR = IntProperty.of("color", 0, 15);
     }
 }
