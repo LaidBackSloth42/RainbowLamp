7d8e161d55b969efe075437873b63c30cae0d5e3
LaidBackSloth42 <109034679+LaidBackSloth@users.noreply.github.com>
2024-09-22 14:22:22 +0200

    update to 1.20

--- src/main/java/com/laidbacksloth/rainbowlamp/BlockRegistry.java
+++ src/main/java/com/laidbacksloth/rainbowlamp/BlockRegistry.java
@@ -4,7 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Material;
+import net.minecraft.block.MapColor;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -17,7 +17,7 @@ import net.minecraft.world.BlockView;
 
 public class BlockRegistry {
     public static final Block RAINBOW_LAMP_BLOCK = Registry.register(Registries.BLOCK, new Identifier(RainbowLamp.MOD_ID, "rainbow_lamp"),
-            new RainbowLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).strength(0.3F).sounds(BlockSoundGroup.GLASS).allowsSpawning(BlockRegistry::always)
+            new RainbowLampBlock(FabricBlockSettings.create().mapColor(MapColor.CLEAR).strength(0.3F).sounds(BlockSoundGroup.GLASS).allowsSpawning(BlockRegistry::always)
                     .luminance((state) -> state.get(RainbowLampBlock.COLOR) == 0 ? 0 : 15)));
 
     public static final Item RAINBOW_LAMP_ITEM = Registry.register(Registries.ITEM, new Identifier(RainbowLamp.MOD_ID, "rainbow_lamp"),
